238aa492429612e7a4f6742b50ff60dded804f5d
murphyj <jmurphy84@googlemail.com>
2017-06-15 14:46:03 +0530

    Added starting code for bowling kata

---
+++ src/main/java/com/cybage/BowlingGame.java
@@ -0,0 +1,17 @@
+package com.cybage;
+
+/**
+ * Created by murphyj on 15/06/2017.
+ */
+public class BowlingGame {
+
+    private int score;
+
+    public int getScore() {
+        return score;
+    }
+
+    public void roll(int numberOfPins) {
+        
+    }
+}
--- src/test/java/com/cybage/BowlingKataTest.java
+++ src/test/java/com/cybage/BowlingKataTest.java
@@ -1,11 +1,23 @@
 package com.cybage;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class BowlingKataTest {
 
     @Test
-    public void DummyTest() throws Throwable {
+    public void atStartOfBowlingGameMyScoreIsZero() throws Throwable {
+        BowlingGame bowlingGame = new BowlingGame();
+        int score = bowlingGame.getScore();
+        assertEquals(0, score);
+    }
 
+    @Test
+    public void knockingDownAPinScoresOne() throws Throwable {
+        BowlingGame bowlingGame = new BowlingGame();
+        bowlingGame.roll(1);
+        int score = bowlingGame.getScore();
+        assertEquals(1, score);
     }
 }
